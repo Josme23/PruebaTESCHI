2a562ac523eb691519a1921291599b5e81dc8a69
Josme23 <ricardomendietajr222@gmail.com>
2023-10-05 21:05:38 -0600

    add

--- APITESCHI/api/models.py
+++ APITESCHI/api/models.py
@@ -2,6 +2,12 @@ from django.db import models
 
 # Create your models here.
 
+class Alumno(models.Model):
+    idAlumno = models.IntegerField(primary_key=True,db_column='idAlumno')
+    nameAlumno = models.CharField(max_length=100,db_column='nameAlumno')
+    class Meta:
+        db_table='Alumnos'
+
 
 
 
